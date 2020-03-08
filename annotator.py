c3cf44fa19d2774d450e5d4901ed1a5e310b0f54
Amartur Rahim Bin Yahya <amarturrahim1795@protonmail.com>
2020-03-09 07:15:11 +0800

    Smarter Annotator

--- annotator.py
+++ annotator.py
@@ -1,59 +1,20 @@
 from colorama import init, Fore
-import json
+import csv
 from nltk.corpus import stopwords
+import re
 import spacy
-from spacy.tokenizer import Tokenizer
-from textblob import TextBlob
-from textblob.classifiers import NaiveBayesClassifier
-from typing import Dict, List
+
+# from textblob import TextBlob
+# from textblob.classifiers import NaiveBayesClassifier
+from typing import List, Tuple, Union
 
 
 class Annotator:
     def __init__(self, db):
         self.db = db
-
         self.nlp = spacy.load("en_core_web_lg")
 
-        self.stop_words = stopwords.words("english")
-        wanted_stopwords = [
-            "up",
-            "down",
-            "not",
-            "off",
-            "haven't",
-            "aren't",
-            "against",
-            "under",
-            "no",
-            "only",
-            "don't",
-            "didn't",
-            "doesn't",
-            "hadn't",
-            "haven't",
-            "isn't",
-            "mightn't",
-            "wasn't",
-            "wouldn't",
-            "hasn't",
-            "until",
-            "won't",
-            "only",
-            "too",
-            "below",
-            "above",
-            "but",
-            "before",
-            "after",
-            "most",
-            "more",
-            "less"
-        ]
-        self.stop_words = [e for e in self.stop_words if e not in wanted_stopwords]
-        unwanted_stopwords = ["we'll", "i'm"]
-        self.stop_words = self.stop_words + unwanted_stopwords
-
-        self.tokenise = Tokenizer(self.nlp.vocab)
+        init()
 
     def _sentencise(self, raw_text: str):
         paragraphed = list(filter(None, raw_text.split("\n")))
@@ -66,161 +27,317 @@ class Annotator:
 
         return sentencised
 
-    def _sanitise(self, sentence: str):
-        unfiltered = [token.text for token in self.tokenise(sentence)]
+    def _load(self, save_file: str) -> Tuple[bool, Union[List[List[str]], None]]:
+        try:
+            data = []
+            with open("./data/" + save_file + ".csv", "r") as csvfile:
+                data = list(csv.reader(csvfile))
 
-        # remove stop words
-        filtered = [word for word in unfiltered if word.lower() not in self.stop_words]
+            return True, data
+        except Exception as e:
+            print(e)
+            return False, None
 
-        return " ".join(word for word in filtered).replace(" - - ","--").replace(" - ","-")
+    def _save(self, save_file: str, data: List[List[str]]) -> bool:
+        try:
+            with open("./data/" + save_file + ".csv", "w", newline="") as csvfile:
+                csv_writer = csv.writer(csvfile)
+                csv_writer.writerows(data)
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
-    def _lemmatise(self, sentences: List[str]):
-        lemmatised_sentences = []
+    def _paragrapher(self, article: str) -> List[str]:
+        # core
+        paragprahs = list(filter(None, article.split("\n")))
+        paragraphs = [paragpraph.strip() for paragpraph in paragprahs]
 
-        for doc in self.nlp.pipe(sentences):
-            lemmatised_sentences.append([token.lemma_ for token in doc])
+        return paragraphs
 
-        for i in range(len(lemmatised_sentences)):
-            lemmatised_sentences[i] = " ".join(
-                lemma for lemma in lemmatised_sentences[i]
-            )
+    def _pre_cleanse_text(self, text: str) -> str:
+        # # core
+        # # TODO: More Stocker Tickers
+        # tickers = ["OTC", "NASDAQ", "NYSE"]
+        # ticker_patterns = ["\\(" + ticker + ":.+?\\)" for ticker in tickers]
 
-        return lemmatised_sentences
+        # for pattern in ticker_patterns:
+        #     text = re.sub(pattern, "", text)
 
-    def _load_file(self, filename: str):
-        return json.load(open("./data/" + filename + ".json", "r"))
+        generic_patterns = [
+            ["\\(.+?:.+?\\)", ""],
+            ["\\.\\.\\.", ""],
+            ["\\([\\w+, !\\?]+\\)", ""],
+            ["  ", " "],
+            [" \\.", "."],
+            [" ,", ","],
+            ["^\\d\\. ", ""],
+            ["U\\.S\\.", "United States"],
+            ["\\.+\\?", ""],
+            ['"', ""],
+            ["\\b\\w+n't\\b", "not"],
+        ]
+        for pattern in generic_patterns:
+            text = re.sub(pattern[0], pattern[1], text)
 
-    def _save_file(self, filename: str, data: List[Dict[str, str]]):
-        with open("./data/" + filename + ".json", "w") as file:
-            json.dump(data, file, indent=2)
-
-    def manual_annotation(self, raw_text: str, filename: str):
-
-        exisiting_list = self._load_file(filename)
-        save = False
-
-        sentences = [
-            self._sanitise(sentence) for sentence in self._sentencise(raw_text)
+        wanted_stopwords = [
+            "above",
+            "after",
+            "against",
+            "before",
+            "below",
+            "but",
+            "down",
+            "less",
+            "more",
+            "most",
+            "no",
+            "nor",
+            "not",
+            "off",
+            "only",
+            "under",
+            "until",
+            "too",
+            "up",
+        ]
+        stop_words = [
+            e for e in stopwords.words("english") if e not in wanted_stopwords
         ]
 
-        sentences = self._lemmatise(sentences)
+        text = [word for word in text.split() if word.lower() not in stop_words]
+        text[0] = text[0].capitalize()
+        text = " ".join(word for word in text)
 
-        sentences = [self._sanitise(sentence) for sentence in sentences]
+        stopword_pattern = ["[Hh]ere", "[TtWw]hat", "[Tt]here"]
+        stopword_pattern = ["\\b" + pattern + "'s\\b" for pattern in stopword_pattern]
+        stopword_pattern += ["\\b\\w+'ll\\b", "\\b[Ii](t|'((ve)|m))\\b", "\\b(([Yy]ou)|([Tt]hey))'re\\b"]
+        for pattern in stopword_pattern:
+            text = re.sub(pattern, "", text)
 
-        init()
+        text = text.replace("  ", " ")
 
-        print("Classifying Statements")
-        print("1: pos, 2: neu, 3: neg, 4: not relevant")
+        return text.strip()
 
-        annotated_list = []
+    def _sentence_segmentor(self, text: str) -> list:
+        # init
+        segmented = []
 
+        # core
+        segmented += [sentence.text.strip() for sentence in self.nlp(text).sents]
+
+        return segmented
+
+    def _post_cleanse_text(self, text: str) -> str:
+        # core
+        generic_patterns = [[".+\\?", ""], [".+vs\\..+", ""], [".*[Ll]et's.*", ""]]
+        for pattern in generic_patterns:
+            text = re.sub(pattern[0], pattern[1], text)
+
+        return text
+
+    def _lemmatise_sentence(self, text: str) -> str:
+        # core
+        text = text.replace(",", "").replace(".", "")
+
+        text = self.nlp(text)
+        text = " ".join([token.lemma_ for token in text])
+
+        return text.strip().replace(" - - ", "--").replace(" - ", "-").replace(" 's", "'s")
+
+    def manual_annotation(self, articles: List[str], save_file: str):
+        # init: load saved annotated data
+        load_success, annotated_data = self._load(save_file)
+        if load_success:
+            print("Load Successful")
+
+        # core
+        # 1: Basic Paragraphing
+        # 2: Pre Cleanse Text
+        # 3: Segment into Sentences via NLP
+        # 4: Post Cleanse Text
+        # 5: Lemmatise Text
+        # 6: Annotate Text
+
+        # 1
+        paragraphed_text = []
+        for article in articles:
+            paragraphed_text += self._paragrapher(article)
+
+        # 2
+        paragraphed_text = [
+            self._pre_cleanse_text(paragraph) for paragraph in paragraphed_text
+        ]
+
+        # 3
+        segmented_text = []
+        for paragraph in paragraphed_text:
+            segmented_text += self._sentence_segmentor(paragraph)
+
+        # 4
+        segmented_text = [
+            self._post_cleanse_text(sentence) for sentence in segmented_text
+        ]
+        segmented_text = list(filter(None, segmented_text))
+
+        # 5
+        segmented_text = [
+            self._lemmatise_sentence(sentence) for sentence in segmented_text
+        ]
+
+        # 6
+        print("Annotate Text")
+
+        green, blue, red, reset = (
+            Fore.GREEN,
+            Fore.BLUE,
+            Fore.RED,
+            Fore.RESET,
+        )  # text colours
+
+        print(
+            green
+            + "0: pos"
+            + reset
+            + ", "
+            + blue
+            + "1: neu"
+            + reset
+            + ", "
+            + red
+            + "2: neg"
+            + reset
+        )
+
+        newly_annotated = []
+        save_data = False
         while True:
-            for sentence in sentences:
-                print(">>> " + sentence + " <<<")
-                d = {}
+            for sentence in segmented_text:
+                print(">> " + sentence)
                 while True:
                     try:
                         x = int(input("> "))
-
-                        if x == 1:
-                            d["text"] = sentence
-                            d["label"] = "pos"
-                            break
-                        elif x == 2:
-                            d["text"] = sentence
-                            d["label"] = "neu"
-                            break
-                        elif x == 3:
-                            d["text"] = sentence
-                            d["label"] = "neg"
-                            break
-                        elif x == 4:
-                            print("Not Relevant")
-                            break
+                        if x < 0 or x > 2:
+                            print("Invalid Range")
                         else:
-                            print("Invalid Input")
+                            break
                     except ValueError:
                         print("Invalid Input")
-                if x == 1 or x == 2 or x == 3:
-                    annotated_list.append(d)
+                newly_annotated.append([sentence, str(x)])
 
-            if annotated_list:
+            if newly_annotated:
                 print("============")
-                for i in range(len(annotated_list)):
-                    label = str(annotated_list[i].get("label"))
-                    text = str(annotated_list[i].get("text"))
-                    if label == "pos":
-                        print(
-                            str(i)
-                            + ": "
-                            + Fore.GREEN
-                            + label
-                            + Fore.RESET
-                            + " > "
-                            + text
-                        )
-                    elif label == "neu":
-                        print(
-                            str(i)
-                            + ": "
-                            + Fore.BLUE
-                            + label
-                            + Fore.RESET
-                            + " > "
-                            + text
-                        )
+
+                for annotation in newly_annotated:
+                    if annotation[1] == "0":
+                        print(green + "pos" + reset + " > " + annotation[0])
+                    elif annotation[1] == "1":
+                        print(blue + "neu" + reset + " > " + annotation[0])
                     else:
-                        print(
-                            str(i) + ": " + Fore.RED + label + Fore.RESET + " > " + text
-                        )
+                        print(red + "neg" + reset + " > " + annotation[0])
 
                 print("============")
-                print("Confirm (Y/y) or Quit(Q/q)?")
-                print("============")
-                try:
-                    y = str(input()).lower()
-                    if y == "y":
-                        save = True
-                        break
-                    elif y == "q":
-                        break
-                except ValueError:
-                    print("Invalid Input. (Reannotation Required)")
-            else:
-                break
+                print("Save (Y/y) or Quit (Q/q)?")
+                print(blue + "press any other button to restart annotation" + reset)
 
-        if save:
-            self._save_file(filename, exisiting_list + annotated_list)
+                i = str(input("> ")).lower()
+                if i == "y" or i == "q":
+                    if i == "y":
+                        save_data = True
+                    break
+                else:
+                    print("Reset Annotations")
 
-    def automatic_annotation(self, raw_text: str, data_file: str, out_file: str):
-        exisiting_list = self._load_file(data_file)
+        # final
+        if save_data:
+            annotated_data += newly_annotated
+        save_success = self._save(save_file, annotated_data)
+        if save_data:
+            print("Save Successful")
+        elif save_success:
+            print("Annotation Terminated")
 
-        sentences = [
-            self._sanitise(sentence) for sentence in self._sentencise(raw_text)
+    def _unannotated_csv_export(self, articles: List[str], save_file: str):
+        # init: load saved annotated data
+        load_success, annotated_data = self._load(save_file)
+        if load_success:
+            print("Import Successful")
+
+        # core
+        # 1: Basic Paragraphing
+        # 2: Pre Cleanse Text
+        # 3: Segment into Sentences via NLP
+        # 4: Post Cleanse Text
+        # 5: Lemmatise
+        # 6: Append Unannotated Data
+
+        # 1
+        paragraphed_text = []
+        for article in articles:
+            paragraphed_text += self._paragrapher(article)
+
+        # 2
+        paragraphed_text = [
+            self._pre_cleanse_text(paragraph) for paragraph in paragraphed_text
         ]
 
-        sentences = self._lemmatise(sentences)
+        # 3
+        segmented_text = []
+        for paragraph in paragraphed_text:
+            segmented_text += self._sentence_segmentor(paragraph)
 
-        sentences = [self._sanitise(sentence) for sentence in sentences]
+        # 4
+        segmented_text = [
+            self._post_cleanse_text(sentence) for sentence in segmented_text
+        ]
+        segmented_text = list(filter(None, segmented_text))
 
-        with open("./data/" + data_file + ".json", "r") as file:
-            classifier = NaiveBayesClassifier(file, format="json")
+        # 5
+        segmented_text = [
+            self._lemmatise_sentence(sentence) for sentence in segmented_text
+        ]
 
-        print("Classifying Statements (Auto)")
+        # 6
+        newly_annotated = []
+        for sentence in segmented_text:
+            newly_annotated.append([sentence, None])
 
-        auto_classified = []
+        annotated_data += newly_annotated
 
-        for i in range(len(sentences)):
+        # final
+        save_success = self._save(save_file, annotated_data)
+        if save_success:
+            print("Export Successful")
 
-            if not sentences[i] or sentences[i] == "happen" or sentences[i] == "\"":
-                continue
+    # def automatic_annotation(self, raw_text: str, data_file: str, out_file: str):
+    #     exisiting_list = self._load_file(data_file)
 
-            d = {}
-            classified = classifier.prob_classify(sentences[i])
-            d["text"] = sentences[i]
-            d["label"] = classified.max()
-            auto_classified.append(d)
+    #     sentences = [
+    #         self._sanitise(sentence) for sentence in self._sentencise(raw_text)
+    #     ]
 
-        exisiting_list = exisiting_list + auto_classified
+    #     sentences = self._lemmatise(sentences)
 
-        self._save_file(out_file, exisiting_list)
+    #     sentences = [self._sanitise(sentence) for sentence in sentences]
+
+    #     with open("./data/" + data_file + ".json", "r") as file:
+    #         classifier = NaiveBayesClassifier(file, format="json")
+
+    #     print("Classifying Statements (Auto)")
+
+    #     auto_classified = []
+
+    #     for i in range(len(sentences)):
+
+    #         if not sentences[i] or sentences[i] == "happen" or sentences[i] == '"':
+    #             continue
+
+    #         d = {}
+    #         classified = classifier.prob_classify(sentences[i])
+    #         d["text"] = sentences[i]
+    #         d["label"] = classified.max()
+    #         auto_classified.append(d)
+
+    #     exisiting_list = exisiting_list + auto_classified
+
+    #     self._save_file(out_file, exisiting_list)
--- main.py
+++ main.py
@@ -4,15 +4,9 @@
 # from textblob import TextBlob
 # from textblob.classifiers import NaiveBayesClassifier
 # from textblob.sentiments import NaiveBayesAnalyzer
-
-# import json
 # import numpy
-from nltk.corpus import stopwords
-import re
-import spacy
-from typing import List
 
-# import annotator as an
+import annotator as an
 import data.db as db
 
 # import data.scraper as sc
@@ -34,134 +28,11 @@ db_handler = db.Db()
 # annotator = an.Annotator(db_handler)
 
 
-def paragrapher(article: str) -> List[str]:
-    paragprahs = list(filter(None, article.split("\n")))
-    paragraphs = [paragpraph.strip() for paragpraph in paragprahs]
-
-    return paragraphs
-
-
-def pre_cleanse_text(text: str) -> str:
-    # init
-    # # TODO: More Stocker Tickers
-    # tickers = ["OTC", "NASDAQ", "NYSE"]
-    # ticker_patterns = ["\\(" + ticker + ":.+?\\)" for ticker in tickers]
-
-    # # core
-    # for pattern in ticker_patterns:
-    #     text = re.sub(pattern, "", text)
-
-    generic_patterns = [
-        ["\\(.+?:.+?\\)", ""],
-        ["\\.\\.\\.", ""],
-        ["\\([\\w+, !\\?]+\\)", ""],
-        ["  ", " "],
-        [" \\.", "."],
-        [" ,", ","],
-        ["^\\d\\. ", ""],
-        ["U\\.S\\.", "United States"],
-        ["\\.+\\?", ""],
-        ['"', ""],
-        ["\\b\\w+n't\\b", "not"],
-    ]
-    for pattern in generic_patterns:
-        text = re.sub(pattern[0], pattern[1], text)
-
-    stop_words = stopwords.words("english")
-    wanted_stopwords = [
-        "above",
-        "after",
-        "against",
-        "before",
-        "below",
-        "but",
-        "down",
-        "less",
-        "more",
-        "most",
-        "no",
-        "nor",
-        "not",
-        "off",
-        "only",
-        "under",
-        "until",
-        "too",
-        "up",
-    ]
-    stop_words = [e for e in stop_words if e not in wanted_stopwords]
-
-    text = [word for word in text.split() if word.lower() not in stop_words]
-    text[0] = text[0].capitalize()
-    text = " ".join(word for word in text).replace(" - - ", "--").replace(" - ", "-")
-
-    stopword_pattern = ["[Hh]ere", "[TtWw]hat", "[Tt]here"]
-    stopword_pattern = ["\\b" + pattern + "'s\\b" for pattern in stopword_pattern]
-    stopword_pattern += ["\\b\\w+'ll\\b", "\\b[Ii]'ve\\b"]
-    for pattern in stopword_pattern:
-        text = re.sub(pattern, "", text)
-
-    text = text.replace("  ", " ")
-
-    return text.strip()
-
-
-def sentence_segmentor(text: str, spacy_model) -> list:
-    # init
-    segmented = []
-
-    # core
-    # TODO: More NLP sentence segmentation libraries
-    # - spaCy
-    segmented += [sentence.text.strip() for sentence in spacy_model(text).sents]
-
-    return segmented
-
-
-def post_cleanse_text(text: str) -> str:
-    generic_patterns = [[".+\\?", ""], [".+vs\\..+", ""], [".*[Ll]et's.*", ""]]
-    for pattern in generic_patterns:
-        text = re.sub(pattern[0], pattern[1], text)
-
-    return text
-
-
 articles = db_handler.read(
     "SELECT content FROM articles.article WHERE url LIKE '%fool.com%';"
 )
 articles = [article[0] for article in articles][:10]
 
-# FLOW
-# 1: Basic Paragraphing
-# 2: Cleanse Text
-# 3: Segment into Sentences via NLP
+annotate = an.Annotator(db)
 
-# 1
-paragraphed_text = []
-for article in articles:
-    paragraphed_text += paragrapher(article)
-
-with open("./data/texts_orig.txt", "w", encoding="UTF-8") as f:
-    for text in paragraphed_text:
-        f.write(text + "\n")
-
-# 2
-paragraphed_text = [pre_cleanse_text(paragraph) for paragraph in paragraphed_text]
-
-with open("./data/texts_ori.txt", "w", encoding="UTF-8") as f:
-    for text in paragraphed_text:
-        f.write(text + "\n")
-
-# 3
-segmented_text = []
-spacy_nlp = spacy.load("en_core_web_lg")
-for paragraph in paragraphed_text:
-    segmented_text += sentence_segmentor(paragraph, spacy_nlp)
-
-segmented_text = [post_cleanse_text(sentence) for sentence in segmented_text]
-segmented_text = list(filter(None, segmented_text))
-
-
-with open("./data/texts.txt", "w", encoding="UTF-8") as f:
-    for sentence in segmented_text:
-        f.write(sentence + "\n")
+annotate._unannotated_csv_export(articles, "annotated_data")
